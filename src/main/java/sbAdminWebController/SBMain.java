c6427b6017d249ecf4cbc54ea1e58a990f213ecf
junkue20 <junkue17@naver.com>
2023-04-19 17:36:56 +0900

    4월19일

--- src/main/java/sbAdminWebController/SBMain.java
+++ src/main/java/sbAdminWebController/SBMain.java
@@ -7,7 +7,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(urlPatterns = {"/index.html"})
+@WebServlet(urlPatterns = {"/adminindex.html"})
 public class SBMain extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
